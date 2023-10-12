bc626cc1a0bfb2ee5ed6d49bc730ef4f643977c1
Ian Sills <iansills04@gmail.com>
2023-10-12 14:40:31 -0500

    added key for APOD

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,29 @@
 console.log("Group - 9  Space Jam X");
 
 
+// fetch for pic of the day
+var exampleURL = 'https://api.nasa.gov/planetary/apod';
+
+	var apiKey ='fPmJ1tNwKaVRT5hG2jPsxrx3q4gF6OSRrzAtx0s8'; 
+    
+	
+    
+    
+    
+    // var request = fetch(); 
+	// request.open('GET', exampleURL + '?api_key=' + apiKey, true);
+
+	// request.addEventListener('load',function(){
+
+	// if(request.status >= 200 && request.status < 400){
+    // var response = JSON.parse(request.responseText);
+	// console.log(response);
+	// } 
+	// else {
+	// console.log("Error in network request: " + request.statusText);
+	// }});
+	// request.send(null);
+					
 
 
 
