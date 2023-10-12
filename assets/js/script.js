7f81737c668b2ef76928f7fefe2be1bfa80bad47
Ian Sills <iansills04@gmail.com>
2023-10-12 15:33:22 -0500

    revamping fetch protocalls

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,32 @@ console.log("Group - 9  Space Jam X");
 
 
 // fetch for pic of the day
+
 var exampleURL = 'https://api.nasa.gov/planetary/apod?api_key=';
 
 	var apiKey ='fPmJ1tNwKaVRT5hG2jPsxrx3q4gF6OSRrzAtx0s8'; 
     
 	fetch(exampleURL + apiKey)
-      .then(data => data.json())
-      .then(data => console.log(data))
+    .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("NETWORK RESPONSE ERROR");
+        }
+      })
+      .then(data => {
+        console.log(data);
+        console.log()
+      })
       .catch(error => console.log('ERROR'));
     
     
+   function displayApod(){
+   
+
+
+   }
+
    
 
 
