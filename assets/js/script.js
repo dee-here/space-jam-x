9f55334990dcf5f08f7dd2a9508e523135435329
Ian Sills <iansills04@gmail.com>
2023-10-12 15:47:09 -0500

    erased javascript out of script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,7 @@
 console.log("Group - 9  Space Jam X");
 
 
-// fetch for pic of the day
 
-var exampleURL = 'https://api.nasa.gov/planetary/apod?api_key=';
-
-	var apiKey ='fPmJ1tNwKaVRT5hG2jPsxrx3q4gF6OSRrzAtx0s8'; 
-    
-	fetch(exampleURL + apiKey)
-    .then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error("NETWORK RESPONSE ERROR");
-        }
-      })
-      .then(data => {
-        console.log(data);
-        displayApod()
-      })
-      .catch(error => console.log('ERROR'));
-    
-    
-   function displayApod(data){
-      
-
-
-   }
-
-   
 
 
 
