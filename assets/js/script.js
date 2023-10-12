8dc23c2a9dcc2c1eafd7abf95e714bd38223f0c6
Ian Sills <iansills04@gmail.com>
2023-10-12 14:47:05 -0500

    apiKey is fetching properly

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ console.log("Group - 9  Space Jam X");
 
 
 // fetch for pic of the day
-var exampleURL = 'https://api.nasa.gov/planetary/apod';
+var exampleURL = 'https://api.nasa.gov/planetary/apod?api_key=';
 
 	var apiKey ='fPmJ1tNwKaVRT5hG2jPsxrx3q4gF6OSRrzAtx0s8'; 
     
