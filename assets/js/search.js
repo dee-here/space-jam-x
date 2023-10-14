39db83be3b4389a9e4c83d0eaf65989279e370fd
Chris San <christianes322@gmail.com>
2023-10-14 16:20:28 -0500

    wrote up inital javascript and linked the api keys and endpoints

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,7 +1,17 @@
+console.log("Search Output!!");
 
-// christians
-fetch("https://images-api.nasa.gov")
+function getEventsFromSearch() {
+  console.log("getEventsFromSearch ");
+
+var apiKey = 'GET /search?q={q}';
+var eventRequestUrl =
+    `https://images-api.nasa.gov${apiKey}`;
+
+
+fetch(eventRequestUrl)
     .then(response => response.json())
     .then(data => console.log(data))
     .catch(error => console.error('Error:', error));
+
+}
 //christians 
